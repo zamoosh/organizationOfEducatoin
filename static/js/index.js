79d3f5dba0a72a132dc804f3b7b1b58f6f0a5cbe
mostafa shariaat <repenter1380@gmail.com>
2022-01-28 23:55:24 +0330

    templates and statics include 3 sections of application login and change password and main page (index.html)

---
+++ static/js/index.js
@@ -0,0 +1,40 @@
+import './utils/sidebar.js';
+import swiperSlider from "./utils/swiperSlider.js";
+
+
+//home page
+
+
+
+
+// swiperSlider("#swiper-article-1", {slidesPerView: [3], direction: "vertical", loop: true});
+// swiperSlider("#swiper-article-2", {slidesPerView: [3], direction: "vertical", loop: true});
+//
+// swiperSlider("#swiper-lesson", {slidesPerView: [1, 2, 3, 4]});
+// swiperSlider("#swiper-notification", {slidesPerView: [1, 2, 3]});
+
+
+
+
+
+
+//lesson page
+
+swiperSlider("#swiper-lesson", {slidesPerView: [1, 2, 3,4] , rows:2 })
+
+
+// swiperSlider(".swiper")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
