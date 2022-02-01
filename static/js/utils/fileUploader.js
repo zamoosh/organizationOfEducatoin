39aaeb78b5e7ce780908b4461fe7cc8c41cdcc23
jafar <jafar0650developer@gmail.com>
2022-02-01 15:25:08 +0330

    نابود گشتم

--- static/js/utils/fileUploader.js
+++
@@ -1,7 +0,0 @@
-const uploader = document.querySelector('.file-uploader');
-const label = document.querySelector('.file-uploader-label')
-uploader.addEventListener('change', (event) => {
-    const value = window.URL.createObjectURL(
-    event.currentTarget.files[0]);
-    label.style.backgroundImage = `url(${value})`;
-});
