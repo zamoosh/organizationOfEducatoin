00323b5166cc4d200db4304918867d2814b926b5
mostafa shariaat <repenter1380@gmail.com>
2022-02-05 00:54:06 +0330

    first commit after create mostafa branch

---
+++ user/views/imports.py
@@ -0,0 +1,4 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render, HttpResponse, get_object_or_404, HttpResponseRedirect, redirect
+from django.urls import reverse
+from django.http import JsonResponse
