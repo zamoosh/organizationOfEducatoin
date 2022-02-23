607b87d690f967573dd46a2519969e55c14d1d9f
Mohammad Rahimi <moyi.pary@gmail.com>
2022-02-23 22:39:24 +0330

    Small change (save user after adding the permission in see_secret.py in lesson app...)

--- lesson/views/see_secret.py
+++ lesson/views/see_secret.py
@@ -5,4 +5,8 @@ def see_secret(request):
     if (request.user.is_staff and request.user.has_perm('lesson.can_drive')) or \
             request.user.has_perm('lesson.can_answer'):
         user = request.user
+        p = Permission.objects.get(name='Can answer question')
+        user.user_permissions.add(p)
+        user.save()
+        print(user.get_user_permissions())
     return HttpResponse('🤑')
