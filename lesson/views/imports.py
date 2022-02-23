f00bf0c36e953166207bd4d4ae2ddf4f1593ff33
Mohammad Rahimi <moyi.pary@gmail.com>
2022-02-23 22:39:24 +0330

    Small change (save user after adding the permission in see_secret.py in lesson app...)

--- lesson/views/imports.py
+++ lesson/views/imports.py
@@ -1,10 +1,10 @@
-from django.core.files.storage import FileSystemStorage
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, HttpResponse, get_object_or_404, HttpResponseRedirect, redirect
 from django.urls import reverse
 from django.http import JsonResponse
 from django.conf import settings
 from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.models import Permission
 from rest_framework.status import HTTP_200_OK
 from lesson.models import *
 from article.models import Article
