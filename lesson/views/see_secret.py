fcf383f7e04c33f4d5393e1ed836832a40b3fe10
Mohammad Rahimi <moyi.pary@gmail.com>
2022-02-24 14:48:35 +0330

    Just edit the /lesson/views/see_secret.py (if was edited)

--- lesson/views/see_secret.py
+++ lesson/views/see_secret.py
@@ -7,9 +7,12 @@ def see_secret(request):
     if (request.user.is_staff and request.user.has_perm('lesson.can_drive')) or \
             request.user.has_perm('lesson.can_answer'):
         user = request.user
-        content_type = ContentType.objects.create(app_label='lesson', model='Lesson')
-        content_type.save()
-        p = Permission.objects.create(content_type=content_type, name='can eat', codename='can_eat')
-        p.save()
-        user.user_permissions.add(p.id)
+        content_type = ContentType.objects.get_or_create(app_label='lesson', model='Lesson')
+        content_type[0].save() if content_type[1] else None
+        permission = Permission.objects.get_or_create(content_type=content_type[0], name='can sleep',
+                                                      codename='can_sleep')
+        permission[0].save() if permission[1] else None
+        user.user_permissions.add(permission[0].id)
+        user.save()
+        print(user.user_permissions.all())
     return HttpResponse('🤑')
