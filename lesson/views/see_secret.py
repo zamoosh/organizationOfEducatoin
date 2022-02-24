737834820b113bab1d38871739aac5ecec929128
Mohammad Rahimi <moyi.pary@gmail.com>
2022-02-24 13:10:57 +0330

    Just see this module: /lesson/views/see_secret.py
    in this module we can:
    1- create a new permission and add it to the user we get from the request
    2- create a new group and add some permission to it if we want...
    3- change permission of user and group if we want...
    4- some other option like that.

--- lesson/views/see_secret.py
+++ lesson/views/see_secret.py
@@ -1,12 +1,15 @@
 from .imports import *
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
 
 
 def see_secret(request):
     if (request.user.is_staff and request.user.has_perm('lesson.can_drive')) or \
             request.user.has_perm('lesson.can_answer'):
         user = request.user
-        p = Permission.objects.get(name='Can answer question')
-        user.user_permissions.add(p)
-        user.save()
-        print(user.get_user_permissions())
+        content_type = ContentType.objects.create(app_label='lesson', model='Lesson')
+        content_type.save()
+        p = Permission.objects.create(content_type=content_type, name='can eat', codename='can_eat')
+        p.save()
+        user.user_permissions.add(p.id)
     return HttpResponse('🤑')
--- organizationOfEducation/settings.py
+++ organizationOfEducation/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     }
 }
 
-# AUTH_USER_MODEL = 'user.Student'
+# AUTH_USER_MODEL = 'lesson.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
